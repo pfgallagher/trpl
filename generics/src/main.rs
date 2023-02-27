6188dc0288bbf267de5bb77ed4abfa40bb7221fa
pfgallagher <pfgallagher@gmail.com>
2023-02-27 10:15:55 -0600

    ch10 progress

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -36,13 +36,13 @@ fn main() {
     // let result = largest(&number_list);
     // println!("The largest number is {}", result);
 
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
     // let integer = Point { x: 5, y: 10 };
     // let float = Point { x: 1.0, y: 4.0 };
@@ -91,7 +91,7 @@ fn main() {
 //     largest
 // }
 
-// fn largest<T>(list: &[T]) -> T {
+// fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
 //     let mut largest = list[0];
 
 //     for &item in list.iter() {
@@ -101,3 +101,14 @@ fn main() {
 //     }
 //     largest
 // }
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list.iter() {
+        if largest < &item {
+            largest = &item;
+        }
+    }
+    &largest
+}
