f689f9831766bae16b3bce488c030e64ac8536b6
pfgallagher <pfgallagher@gmail.com>
2023-03-09 10:10:46 -0600

    ch15 progress

---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,69 @@
+use std::ops::Deref;
+use List::{Cons, Nil};
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    // let b = Box::new(5);
+    // println!("b = {}", b);
+
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // let x = 5;
+    // let y = &x;
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    // let x = 5;
+    // let y = Box::new(x);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    // let x = 5;
+    // let y = MyBox::new(x);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    // let m = MyBox::new(String::from("Rust"));
+    // hello(&m);
+
+    // let c = CustomSmartPointer { data: String::from("my stuff")};
+    // let d = CustomSmartPointer { data: String::from("other stuff")};
+    // println!("CustomSmartPointers created.");
+    
+    let c = CustomSmartPointer { data: String::from("some data")};
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name)
+}
