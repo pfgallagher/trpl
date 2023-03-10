5ac7a55dbddf812c6036316e5ae8b647564e534c
pfgallagher <pfgallagher@gmail.com>
2023-03-10 11:04:29 -0600

    ch15 progress

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,7 +1,11 @@
-use std::ops::Deref;
+use std::rc::Rc;
+use std::{cell::RefCell, ops::Deref};
 use List::{Cons, Nil};
+
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -57,11 +61,37 @@ fn main() {
     // let c = CustomSmartPointer { data: String::from("my stuff")};
     // let d = CustomSmartPointer { data: String::from("other stuff")};
     // println!("CustomSmartPointers created.");
-    
-    let c = CustomSmartPointer { data: String::from("some data")};
-    println!("CustomSmartPointer created.");
-    drop(c);
-    println!("CustomSmartPointer dropped before the end of main.");
+
+    // let c = CustomSmartPointer { data: String::from("some data")};
+    // println!("CustomSmartPointer created.");
+    // drop(c);
+    // println!("CustomSmartPointer dropped before the end of main.");
+
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, Rc::clone(&a));
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    // let x = 5;
+    // let y = &mut x;
+
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+    *value.borrow_mut() += 10;
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
 fn hello(name: &str) {
